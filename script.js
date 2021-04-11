167255fc3cadc23cb854d32a676a5bcf12263fbe
firte07 <firte.catalin@gmail.com>
2021-04-11 23:21:27 +0300

    Done

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleMouseOver(x){
+    x.style.color = "blue";
+    document.body.style.cursor = "pointer";
+  }
+
+function handleMouseOut(x){
+    x.style.color = "black";
+    document.body.style.cursor = "default";
+}
