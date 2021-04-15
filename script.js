160a376e47d1c3a9cc417e6a41e8349ec5e25862
firte07 <firte.catalin@gmail.com>
2021-04-15 10:51:22 +0300

    Add classes for native HTML components, put spaces after the name of the clasess, indent HTML code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function handleMouseOver(textFromInfo){
-    textFromInfo.style.color = "blue";
-    document.body.style.cursor = "pointer";
-  }
+  textFromInfo.style.color = "blue";
+  document.body.style.cursor = "pointer";
+}
 
 function handleMouseOut(textFromInfo){
-    textFromInfo.style.color = "black";
-    document.body.style.cursor = "default";
+  textFromInfo.style.color = "black";
+  document.body.style.cursor = "default";
 }
